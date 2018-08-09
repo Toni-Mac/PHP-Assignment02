81e0eb99308b3384e8ac25f2239e79239b3f8811
ystubbs91 <40002944+ystubbs91@users.noreply.github.com>
2018-08-09 12:56:11 -0700

    add page and styles

---
+++ add-user-page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles/styles.css"/>
+    <title>Add User</title>
+</head>
+<body>
+<div class="wrapper">
+<h1>Assignment 02 | Toni McIntire &amp; Yadira Stubbs</h1>
+<h3>Add A Student</h3>
+
+<form method="POST" action="user_processing.php">
+<input type="hidden" name="add" value"add">
+<input type="text" name="studentnumber" id="studentnumber">
+<label for="studentnumber"> -  Student Number</label>
+<br>
+<input type="text" name="firstname" id="firstname">
+<label for="firstname"> - Firstname</label>
+<br>
+<input type="text" name="lastname" id="lastname">
+<label for="lastname"> - Lastname</label>
+<br>
+
+<input type="submit" value="Submit">
+</form>
+</div>
+    
+</body>
+</html>
